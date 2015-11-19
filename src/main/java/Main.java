0368844a454ba1e5c2f800889d25ef3281bbcbee
Ruoyu Zou <ruoyu@groupon.com>
2015-11-19 10:27:10 -0800

    initial check in

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ruoyu on 11/19/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/main/java/ParseClient.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ruoyu on 11/19/15.
+ */
+public class ParseClient {
+}
